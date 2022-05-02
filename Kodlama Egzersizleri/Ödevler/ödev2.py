1b611709baf59af68ad226d05dd3209bfd840d21
WMPRX <wampirnsb@gmail.com>
2022-05-03 00:51:24 +0300

    Delete Kodlama Egzersizleri/Ödevler directory

--- Kodlama Egzersizleri/Ödevler/ödev2.py
+++
@@ -1,6 +0,0 @@
-# Kullanıcıdan aldığınız boy ve kilo değerlerine göre kullanıcının beden kitle indeksini bulun.
-
-b = float(input("boy: "))
-a = int(input("kilo: "))
-
-print("bedn kitle indexi:",a / (b ** 2))
--- Kodlama Egzersizleri/Ödevler/ödev3.py
+++
@@ -1,6 +0,0 @@
-#Bir aracın kilometrede ne kadar yaktığı ve kaç kilometre yol yaptığı bilgilerini alın ve sürücünü toplam ne kadar ödemesini gerektiğini hesaplayın.
-
-a = float(input("kilometre başı yakım: "))
-b = int(input("kaç kilometre yol yaptı: "))
-
-print("Tutar {} tl yakmıştır".format(a*b))
