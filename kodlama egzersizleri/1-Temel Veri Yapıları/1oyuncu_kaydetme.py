9e98c6e0a614ff6580c394a727a756f246d23f41
WMPRX <wampirnsb@gmail.com>
2022-05-03 00:59:52 +0300

    Add files via upload

---
+++ kodlama egzersizleri/1-Temel Veri Yapıları/1oyuncu_kaydetme.py
@@ -0,0 +1,14 @@
+print("Oyuncu Kaydetme Programı")
+
+ad= input("oyuncu adı: ")
+soyad = input("oyuncu soyadı: ")
+takım = input("oyuncu takımı: ")
+
+bilgiler = [ad,soyad,takım]
+
+print("Bilgiler kaydediliyor...")
+
+print("oyuncu adı: {}\n oyuncu soyadı: {}\n Oyuncu takımı: {}\n".format(bilgiler[0], bilgiler[1], bilgiler[2]))
+
+print("Bilgiler kaydedildi...")
+
