5639df262d648c8281bb08d3c0312fc503a6d208
WMPRX <wampirnsb@gmail.com>
2022-05-03 00:49:48 +0300

    Add files via upload

---
+++ Kodlama Egzersizleri/Ödevler/ödev2.py
@@ -0,0 +1,6 @@
+# Kullanıcıdan aldığınız boy ve kilo değerlerine göre kullanıcının beden kitle indeksini bulun.
+
+b = float(input("boy: "))
+a = int(input("kilo: "))
+
+print("bedn kitle indexi:",a / (b ** 2))
---
+++ Kodlama Egzersizleri/Ödevler/ödev4.py
@@ -0,0 +1,9 @@
+#Kullanıcıdan ad,soyad ve numara bilgisini alarak bunları alt alta ekrana yazdırın.
+
+isim = input("İsim: ")
+soyİsim = input("Soyisim: ")
+numara = input("Telefon numarası: ")
+
+print("\nbilgiler.....\n")
+
+print("isim: {}\nsoyisim: {}\nnumara: {}\n".format(isim,soyİsim,numara))
